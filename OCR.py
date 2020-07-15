577cc318d52625914976f61c59b5efd25e83dd96
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-07-15 18:05:00 +0100

    Update OCR.py
    prety print updated

--- OCR.py
+++ OCR.py
@@ -3,11 +3,12 @@ from uuid import uuid4
 import qrcode
 import pytesseract
 import sys
+from pprint import pprint
 pytesseract.pytesseract.tesseract_cmd = r'tesseract.exe'
 
 img=Image.open(sys.argv[1],'r')
 
 text=pytesseract.image_to_string(img,lang="eng")
 
-print(text)
+pprint(text)
 
